a324460758ab64ce42b4ef2637ea3b3e475aa585
Alex <belova.al.ig@gmail.com>
2022-07-09 22:40:52 +0300

    Added Conditions If/Else

---
+++ Example005_HelloMasha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Insert your name: ");
+string username = Console.ReadLine( );
+if(username == "Masha")
+{
+    Console.Write("Yep, that's Masha");
+}
+else
+{
+Console.Write("Hello, ");
+Console.Write(username);
+}
+
