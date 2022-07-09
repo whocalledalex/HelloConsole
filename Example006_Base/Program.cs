ca36d796f796f5926744e73c0574eb53c2882b9a
Alex <belova.al.ig@gmail.com>
2022-07-09 23:02:07 +0300

    Added exercise with weights

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 2;
+int b = 5;
+int c = 43;
+int d = 6;
+int e = 9;
+
+int max = a;
+
+if(a>max) max = a;
+if(b>max) max = b;
+if(c>max) max = c;
+if(d>max) max = d;
+if(e>max) max = e;
+
+Console.Write("max = ");
+Console.Write(max);
