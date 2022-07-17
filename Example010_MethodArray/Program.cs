9517547902a0585826b251a8c7d57eec7bb2d781
Alex <belova.al.ig@gmail.com>
2022-07-17 22:32:28 +0300

    Program is working

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = { 1, 6, 23, 65, 21, 124, 4, 2345, 2358, 2235 };
+
+int n = array.Length;
+int find = 2235;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+    Console.WriteLine(index);
+    break;
+    }
+
+    //index = index + 1;
+    index++;
+    
+}
